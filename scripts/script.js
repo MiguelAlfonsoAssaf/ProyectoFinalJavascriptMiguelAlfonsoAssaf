17d41ed7ca9bbde6a7702e8fce5e4f1bf47acf82
Miguel Alfonso Assaf <massaf_tuc@hotmail.com>
2022-12-21 19:12:56 -0300

    se agrego la generacion de nodos

--- scripts/script.js
+++ scripts/script.js
@@ -22,17 +22,53 @@ class producto {
     agregoCantidad(num){
         this.cantidad=this.cantidad+num;
     }
+    agregoUno(){
+        this.cantidad=this.cantidad+1;
+    }
 }
 
+//array de productos:
+let carrito=[];
 
 
+
+//agrego eventos a los botones con la clase button-62
 let botas=document.getElementsByClassName("button-62");
-
-
 for (const bota of botas) {
     bota.addEventListener("click",agregoCarrito);
 }
+// Funcion que dispara el evento:
+//chequeo si existe el zapato en la factura, si existe => sumo 1 a la cantidad.
+//                                        si no existe => agrego un objeto al carrito.
 
-function agregoCarrito(){
-    console.log("se agrego al carrito");
+function agregoCarrito(event){
+    if(carrito.some((elem)=>elem.nombre==event.target.value)){
+        const indiceCarrito= (elem) => elem.nombre==event.target.value;
+        let indice = carrito.findIndex(indiceCarrito);
+        carrito[indice].agregoUno();
+        agregoCarritoRepetido(carrito[indice]);
+
+    } else {
+        let elemento = lista_productos.find((elem) => elem.nombre == event.target.value);
+        let nuevoproducto=new producto(elemento.nombre,elemento.precio,1);
+        agregoCarritoHTML(nuevoproducto);
+        carrito.push(nuevoproducto);
+    }
+    console.log(carrito);
+}
+
+
+// funcion que agrega un nodo "p" a la seccion item_compras
+function agregoCarritoHTML(producto){
+let divitems=document.getElementById("items_compra");
+let item=document.createElement("p");
+item.innerText=producto.nombre + "\t" + " x " + producto.cantidad + "\t .Precio Unidad:" + producto.precio;
+item.setAttribute("id",producto.nombre)
+divitems.appendChild(item);
+}
+
+// si existe el nodo dentro de item_compras se actualiza el innertext para agregar la cantidad.
+function agregoCarritoRepetido(producto){
+    let itemrepetido=document.getElementById(producto.nombre);
+    itemrepetido.innerText=producto.nombre + "\t" + " x " + producto.cantidad + "\t .Precio Unidad:" + producto.precio;
 }
